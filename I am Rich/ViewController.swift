de1153d6c89f7115ad62171d3c6c99b7de16163d
safarovsafar <safarov.safar@hotmail.com>
2020-06-28 03:33:04 +0800

    test

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Safar Safarov on 2020/6/5.
+//  Copyright © 2020 Safarov Safar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
